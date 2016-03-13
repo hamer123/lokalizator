7038aee5b37ce777fc4404996ee9938dd5d1fee2
Patryk <Patryk@Patryk-Komputer>
2016-03-13 16:58:51 +0100

    q

---
+++ src/main/java/com/pw/lokalizator/rest/UserRest.java
@@ -0,0 +1,44 @@
+package com.pw.lokalizator.rest;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+
+import com.pw.lokalizator.model.User;
+import com.pw.lokalizator.repository.Dao;
+import com.pw.lokalizator.repository.UserDao;
+
+@Stateless
+@Path("/user")
+public class UserRest {
+
+	@EJB
+	private Dao userDao;
+	
+	@GET
+	@Path("/create")
+	public Response createUser(@QueryParam("login") String login, 
+			                   @QueryParam("password") String password,
+			                   @QueryParam("email") String email){
+		
+		User user = new User();
+		user.setLogin(login);
+		user.setPassword(password);
+		user.setEmail(email);
+		try{
+			userDao.persist(user);
+		}catch(Exception e){
+			return Response
+					.ok("NIE DODANO")
+					.build();
+		}
+		
+		return Response
+				.ok("DODANO")
+				.build();
+	}
+
+}
