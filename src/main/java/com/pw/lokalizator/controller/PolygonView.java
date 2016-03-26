e5c9b77df939158759c145460b57fa5e2f78134c
Patryk <Patryk@Patryk-Komputer>
2016-03-26 23:15:20 +0100

    q

--- src/main/java/com/pw/lokalizator/controller/PolygonView.java
+++ src/main/java/com/pw/lokalizator/controller/PolygonView.java
@@ -2,18 +2,13 @@ package com.pw.lokalizator.controller;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
-import javax.faces.event.ActionEvent;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
-
 import org.primefaces.event.map.PointSelectEvent;
 import org.primefaces.model.map.DefaultMapModel;
 import org.primefaces.model.map.LatLng;
@@ -27,14 +22,13 @@ public class PolygonView implements Serializable{
 	private LokalizatorSession lokalizatorSession;
 	
 	private MapModel polygonModel;
-	private Map<Integer, LatLng>points;
-	private Polygon polygon;
+	private List<LatLng>points;
 	
 	@PostConstruct
 	private void postConstruct(){
 		polygonModel = new DefaultMapModel();
-		points = new HashMap<Integer, LatLng>();
-		polygon = new Polygon();
+		points = new ArrayList<LatLng>();
+		Polygon polygon = new Polygon();
 		
         polygon.setStrokeColor("#FF9900");
         polygon.setFillColor("#FF9900");
@@ -46,10 +40,16 @@ public class PolygonView implements Serializable{
 	
 	public void onPointSelect(PointSelectEvent event){
         LatLng latlng = event.getLatLng();
-        points.put(points.size() + 1, latlng);
-        polygon.getPaths().add(latlng);
-        
+        points.add(latlng);
+        polygonModel.getPolygons().get(0).getPaths().add(latlng);
+
         FacesContext.getCurrentInstance().addMessage(null, (new FacesMessage(FacesMessage.SEVERITY_INFO, "Point Selected", "Lat:" + latlng.getLat() + ", Lng:" + latlng.getLng())));
+	}
+	
+	public void actionRemove(String index){
+		System.err.println("REMOVE " + index);
+		points.remove( Integer.parseInt(index) - 1 );
+		polygonModel.getPolygons().get(0).getPaths().remove( Integer.parseInt(index) - 1 );
 	}
 
 	public MapModel getPolygonModel() {
@@ -60,11 +60,11 @@ public class PolygonView implements Serializable{
 		this.polygonModel = polygonModel;
 	}
 
-	public Map<Integer, LatLng> getPoints() {
+	public List<LatLng> getPoints() {
 		return points;
 	}
 
-	public void setPoints(Map<Integer, LatLng> points) {
+	public void setPoints(List<LatLng> points) {
 		this.points = points;
 	}
 	
